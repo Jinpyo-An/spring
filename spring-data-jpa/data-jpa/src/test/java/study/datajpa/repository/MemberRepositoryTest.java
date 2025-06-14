8c4a153cc8855434019ec2da48759ee1888d8855
Jinpyo An <dkswlsvy3312@gmail.com>
2025-06-14 17:06:12 +0900

    공통 인터페이스 기능

--- spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,13 +1,14 @@
 package study.datajpa.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,15 +19,27 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    public void testMember() {
-        final Member member = new Member("memberA");
+    public void basicCRUD() {
+        final Member member1 = new Member("member1");
+        final Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
-        final Member savedMember = memberRepository.save(member);
+        final Member findMember1 = memberRepository.findById(member1.getId()).get();
+        final Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
 
-        final Member findMember = memberRepository.findById(savedMember.getId()).get();
+        final List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
 
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        assertThat(findMember).isEqualTo(member);
+        final Long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        final Long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
